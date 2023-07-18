9ce2132d7afed4682142f92d58d8ccdc71655395
ghosind <ghosind@gmail.com>
2023-07-18 22:19:41 +0800

    chore: remove unused error.

--- error.go
+++ error.go
@@ -2,7 +2,7 @@ package request
 
 import "errors"
 
-// ErrNoURL throws when no uri and base url set in the request.
-var ErrNoURL error = errors.New("no url")
-
-var ErrUnsupportedContentType = errors.New("unsupported content type")
+var (
+	// ErrNoURL throws when no uri and base url set in the request.
+	ErrNoURL error = errors.New("no url")
+)
