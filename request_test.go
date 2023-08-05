b88fab717fa83f50b671896ab5558cbed0a998b1
ghosind <ghosind@gmail.com>
2023-08-05 23:20:19 +0800

    test: add test case for getContext.

--- request_test.go
+++ request_test.go
@@ -1,9 +1,11 @@
 package request
 
 import (
+	"context"
 	"net/http"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/ghosind/go-assert"
 )
@@ -25,4 +27,39 @@ func TestGetRequestMethod(t *testing.T) {
 
 	_, err = cli.getRequestMethod("UNKNOWN")
 	a.NotNil(err)
+}
+
+func TestGetContext(t *testing.T) {
+	a := assert.New(t)
+	cli := New()
+
+	baseCtx := context.Background()
+	ctx, _ := cli.getContext(RequestOptions{
+		Context: baseCtx,
+	})
+	a.DeepEqual(ctx, baseCtx)
+
+	ctx, _ = cli.getContext(RequestOptions{})
+	deadline, ok := ctx.Deadline()
+	a.DeepEqualNow(ok, true)
+	a.DeepEqualNow((deadline.UnixMilli() - time.Now().UnixMilli()), int64(1000))
+
+	ctx, _ = cli.getContext(RequestOptions{
+		Timeout: 3000,
+	})
+	deadline, ok = ctx.Deadline()
+	a.DeepEqualNow(ok, true)
+	a.DeepEqualNow((deadline.UnixMilli() - time.Now().UnixMilli()), int64(3000))
+
+	ctx, _ = cli.getContext(RequestOptions{
+		Timeout: RequestTimeoutNoLimit,
+	})
+	_, ok = ctx.Deadline()
+	a.DeepEqualNow(ok, false)
+
+	cli.timeout = 3000
+	ctx, _ = cli.getContext(RequestOptions{})
+	deadline, ok = ctx.Deadline()
+	a.DeepEqualNow(ok, true)
+	a.DeepEqualNow((deadline.UnixMilli() - time.Now().UnixMilli()), int64(3000))
 }
