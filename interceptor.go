2cfae9ee5038b64f61a27adb9865c15a99de4380
ghosind <ghosind@gmail.com>
2023-11-08 23:15:07 +0800

    feat: execute request and response interceptors.

--- interceptor.go
+++ interceptor.go
@@ -7,3 +7,39 @@ type RequestInterceptor func(*http.Request) error
 
 // ResponseInterceptor is a function to intercept the responses.
 type ResponseInterceptor func(*http.Response) error
+
+// doRequestIntercept executes the request interceptors, it'll terminate if the interceptor
+// function returns an error.
+func (cli *Client) doRequestIntercept(req *http.Request) error {
+	interceptors := cli.reqInterceptors
+	if len(interceptors) == 0 {
+		return nil
+	}
+
+	for _, interceptor := range interceptors {
+		err := interceptor(req)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// doResponseIntercept executes the response interceptors, it'll terminate if the interceptor
+// function returns an error.
+func (cli *Client) doResponseIntercept(resp *http.Response) error {
+	interceptors := cli.respInterceptors
+	if len(interceptors) == 0 {
+		return nil
+	}
+
+	for _, interceptor := range interceptors {
+		err := interceptor(resp)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
